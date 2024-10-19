672d0be7af82489a8a7ec9563c51977f54559e4a
Gabriel Lima <gabriellimadeandrade4@gmail.com>
2024-10-19 11:21:36 -0300

    Primeiro arquivo JAVA

--- Documents/Java/main.java
+++
@@ -1,9 +0,0 @@
-//classe principal que deve ter o mesmo nome de arquivo
-public class main {
-  public static void main(String[] args) { //tudo que estiver dentro dessa linha sera executado
-    //comandos para executar mensagem na tela
-    System.out.println("Hello World");
-    System.out.print("TEXTO ABROBINHA");
-    System.out.print("TERMINA AI");
-  }
-}
