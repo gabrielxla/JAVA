4e460646ce35eb521ae7f6f4933581d21901d11c
Gabriel Lima <gabriellimadeandrade4@gmail.com>
2024-10-20 00:22:22 -0300

    Teste com variaveis

---
+++ Variaveis.java
@@ -0,0 +1,16 @@
+public class Variaveis {
+    public static void main(String[] args) {
+      String name = "soper";
+      System.out.println(name);
+      int myNum = 15;
+      System.out.println(myNum);
+      String Primeironome = "Gabriel ";
+      String Segundonome = "Lima";
+      String Nomecompleto = Primeironome + Segundonome ;
+      System.out.println(Nomecompleto);
+      int x = 1;
+      int y = 1;
+      System.out.println ( x + y );
+    }
+  }
+  
